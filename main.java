a5a26b63d4e215f67c20674633065abf5c687e9e
aditaurog-stack <adi.taurog@gmail.com>
2025-11-27 12:44:34 +0200

    Add FavoriteBooks and FavoriteArtists classes

--- main.java
+++ main.java
@@ -20,7 +20,54 @@ public class FavoriteMovies {
             System.out.println("- " + movie);
         }
 
+        import java.util.Scanner;
+
+public class FavoriteBooks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many favourite books do you want to enter?");
+        int count = scanner.nextInt();
+        scanner.nextLine(); // Clear leftover newline
+
+        String[] books = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("Enter book #" + (i + 1) + ": ");
+            books[i] = scanner.nextLine();
+        }
+
+        System.out.println("\nYour favourite books are:");
+        for (String book : books) {
+            System.out.println("- " + book);
+        }
+
         scanner.close();
     }
 }
+
+
+        scanner.close();
+    }
+}
+import java.util.Scanner;
+
+public class FavoriteArtists {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many favourite artists do you want to enter?");
+        int count = scanner.nextInt();
+        scanner.nextLine(); // Clear leftover newline
+
+        String[] artists = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("Enter artist #" + (i + 1) + ": ");
+            artists[i] = scanner.nextLine();
+        }
+
+        System.out.println("\nYour favourite artists are:");
+        for (String artist : artists) {
+            System.out.println("- " + art
 
